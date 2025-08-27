4c9df5dcafada3f95bf2c3cd5abedd0b67748fbd
tilliedstrom <tilliedstrom@elev.ga.ntig.se>
2025-08-27 10:34:35 +0200

    test text

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 // nano server.js
 
+
+// let - can change
+// const - should always be the same
+
 import express from "express"
 
 const app = express()
 
 app.get("/", (req, res) => {
     res.send("Hello world")
+    res.send("Wappy life wappy wife")
 })
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port http://localhost:3000")
 })
 

b6e76a3efa3c2225a0c19dbd6604720ff6c21421
tilliedstrom <tilliedstrom@elev.ga.ntig.se>
2025-09-02 14:13:44 +0200

    lektion 2

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
-// nano server.js
 
-
-// let - can change
-// const - should always be the same
-
-import express, { json } from "express"
+import express from "express";
+import nunjucks from "nunjucks"
 
 const app = express()
 
+app.use(express.static("public"))
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app
+})
+
 app.get("/", (req, res) => {
-    res.write("Wappy life wappy wife \n")
-    res.write("Hello beautiful, I will teach you the most important thing in the world... \n")
-    res.write("Cookies may taste sweet but they'll eventually rot your teeth, be careful of what you eat \n")
-    res.end("Thank you! Mashallah, Namaste, Amitofo, God Bless your heart and live on beautifully")
+    res.render("index.njk", {
+        title: "Our first website",
+        message: "With nunjucks we are creating"
+    })
 })
 
 app.listen(3000, () => {
     console.log("Server is running on port http://localhost:3000")
-})
-
-app.get("/about", (req, res) => {
-    res.json ({
-        "messege": "Hehe"
-    })
-})
-
-app.get("/secret", (req, res) => {
-    res.send("Heh göj")
 })
 

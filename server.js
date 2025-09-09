51a29dea2625abc4c1b1de287bc07fc370118e09
tilliedstrom <tilliedstrom@elev.ga.ntig.se>
2025-09-09 09:57:19 +0200

    lesson 3

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 
 import express from "express";
 import nunjucks from "nunjucks"
+import morgan from "morgan"
 
 const app = express()
 
+export default function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("img")
+}
+
+app.use(morgan("dev"))
 app.use(express.static("public"))
 
 nunjucks.configure("views", {
@@ -11,14 +17,26 @@ nunjucks.configure("views", {
     express: app
 })
 
-app.get("/", (req, res) => {
+app.get("/index", (req, res) => { //Route
     res.render("index.njk", {
         title: "Our first website",
         message: "With nunjucks we are creating"
     })
 })
 
+app.get("/about", (req, res) => {
+    res.render("about.njk", {
+        title: "Hello",
+        message: "Yay"
+    })
+})
+
 app.listen(3000, () => {
     console.log("Server is running on port http://localhost:3000")
+})
+
+app.get("/greetings", (req, res) => {
+    console.log(req.query)
+    res.send(`Hallå ${req.query.name}, ${req.query.messege}`)
 })
 
